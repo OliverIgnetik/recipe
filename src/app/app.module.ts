cf717f08a6bd6d5500e47440320043e3c7cb4b4b
Oliver Ignetik <olli@ozemail.com.au>
2019-08-12 17:22:54 +1000

    Added navbar and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// services
+import {RecipeService} from './services/recipe.service';
+
+// components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditRecipeComponent } from './components/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './components/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditRecipeComponent,
+    NewRecipeComponent,
+    ShowRecipeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    RecipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
